5131abf36427c6a2655b3e56b6f6439caf4c0ae8
João Menezes <joao.menezes@ccc.ufcg.edu.br>
2019-08-15 14:40:24 -0300

    rotas do crud doador

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,10 +4,14 @@ const ItemController = require('./controllers/ItemController');
 
 const routes = express.Router();
 
-routes.get('/doador/:usuarioId', DoadorController.pesquisaUsuarioPorId);
+routes.get('/doador/id/:id', DoadorController.pesquisaUsuarioPorId);
+routes.get('/doador/nome/:nome', DoadorController.pesquisaUsuarioPorNome);
 
 routes.post('/doador', DoadorController.adicionaDoador);
 
+routes.put('/doador/atualiza/:id', DoadorController.atualizaUsuario);
+
+routes.delete('/doador/remove/:id', DoadorController.removeUsuario);
 
 routes.post('/item', ItemController.adicionarItem);
 
