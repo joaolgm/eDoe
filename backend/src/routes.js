3f7bc55e4290d10423e811fc310d7d15b232e4a5
João Menezes <joao.menezes@ccc.ufcg.edu.br>
2019-08-28 22:18:27 -0300

    adequando as rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,26 +2,29 @@ const express = require('express');
 const DoadorController = require('./controllers/DoadorController');
 const ReceptorController = require('./controllers/ReceptorController');
 const ItemController = require('./controllers/ItemController');
+const DoaController = require('./controllers/DoaController');
 
 const routes = express.Router();
 
-routes.get('/doador/id/:id', DoadorController.pesquisaUsuarioPorId);
-routes.get('/doador/nome/:nome', DoadorController.pesquisaUsuarioPorNome);
+routes.get('/usuario/id/:id', DoadorController.pesquisaUsuarioPorId);
+routes.get('/usuario/nome/:nome', DoadorController.pesquisaUsuarioPorNome);
 routes.get('/item-descritores', ItemController.listaDescritorDeItensParaDoacao);
 routes.get('/item', ItemController.listaItensParaDoacao);
-routes.get('/matching', ReceptorController.matching);
+routes.get('/doacao', DoaController.historicoDoacao);
 
 routes.post('/doador', DoadorController.adicionaDoador);
 routes.post('/receptor', ReceptorController.adicionaReceptor);
 routes.post('/item', DoadorController.adicionaItem);
 routes.post('/item-necessario', ReceptorController.adicionaItemNecessario);
+routes.post('/matching', ReceptorController.matching);
 
-routes.put('/doador/:id', DoadorController.atualizaUsuario);
+routes.put('/doador/:id', DoadorController.atualizaDoador);
 routes.put('/receptor', ReceptorController.atualizaReceptor);
 routes.put('/item-necessario', ReceptorController.atualizaItemNecessario);
 
-routes.delete('/doador/:id', DoadorController.removeUsuario);
+routes.delete('/usuario/:id', DoadorController.removeUsuario);
 routes.delete('/item', DoadorController.removeItem);
 routes.delete('/item-necessario', ReceptorController.removeItemNecessario);
+routes.delete('/doacao', DoaController.realizaDoacao);
 
 module.exports = routes;
